b0193e31e9ffbd763dca56f2b9d6b52644f99f21
DiraBootDotDev <ahmad.houran+BootDotDev@gmail.com>
2026-06-04 15:53:41 +0200

    Delete tests directory

--- tests/environment/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Tests for the optiland.environment subpackage.
-
-This package contains unit tests for environmental modeling utilities,
-such as air refractive index calculations.
-"""
--- tests/test_fileio/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Tests for optiland.fileio."""
-
-from __future__ import annotations
